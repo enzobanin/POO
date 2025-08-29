e13e228fdb8f20c596433f9cdf43e0e69a2255fc
enzobanin <e.banin@aluno.ifsp.edu.br>
2025-08-28 22:04:01 -0300

    Ex1-Feito

--- ListaExercicios/ExUm.java
+++ ListaExercicios/ExUm.java
@@ -1,3 +1,26 @@
 public class ExUm {
-    
+    public static void main(String[] args) {
+        int x = 60000;
+        if(args.length>0){
+            switch(Integer.parseInt(args[0])){
+                case 5: 
+                    x = x + 1500;
+                    
+                case 4: 
+                    x = x + 5000;
+                    
+                case 3: 
+                    x = x + 2500;
+                    
+                case 2: 
+                    x = x + 4000;
+                    
+                case 1: 
+                    x = x + 7000;
+                default:
+                    System.out.println("O carro " + args[0] + " é igual a " + x);
+                    break;
+            }
+        }
+    }
 }
